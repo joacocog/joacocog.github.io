9cde3c983cc865cd085f44c5ddbb11a2a0c39838
joacocog <joacocog@icloud.com>
2023-04-23 21:42:09 -1000

    React-App, navBar ready

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import "./NavBarStyles.css";
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import {FaBars, FaTimes} from "react-icons/fa";
+
+const NavBar = () => {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+
+  return (
+    <div className="header">
+        <Link to="mailto:joacocog@icloud.com"><h2>joacocog@icloud.com</h2></Link>
+        <ul className={click ? ("nav-menu active") : ("nav-menu")}>
+            <li>
+                <Link to="#">Home</Link>
+            </li>
+            <li> 
+                <Link to="#">About me</Link>
+            </li>
+            <li>
+                <Link to="#">Skills</Link>
+            </li>
+            <li>
+                <Link to="#">Projects</Link>
+            </li>
+            <li>
+                <Link to="#">Contact</Link>
+            </li>  
+        </ul>
+        <div className="burguer-menu" onClick={handleClick}>
+            {click ? (<FaTimes size={20} style={{color: '#fff'}}/>)
+            : (<FaBars size={20} style={{color: '#fff'}}/>)}
+            
+            
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
