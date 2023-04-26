71223ca35a83f07b9f5f945d74a3dbe0d57b2708
joacocog <joacocog@icloud.com>
2023-04-25 17:26:15 -1000

    NavBar Color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,22 @@ import {FaBars, FaTimes} from "react-icons/fa";
 const NavBar = () => {
 
     const [click, setClick] = useState(false);
-
     const handleClick = () => setClick(!click);
+
+    const [color, setColor] = useState(false);
+    const changeColor = () => {
+        if(window.scrollY >=100){
+            setColor(true);
+        }else{
+            setColor(false); 
+        }
+    }
+
+    window.addEventListener("scroll", changeColor);
 
 
   return (
-    <div className="header">
+    <div className={color ? "header header-bg" : "header"}>
         <Link to="mailto:joacocog@icloud.com"><h2>joacocog@icloud.com</h2></Link>
         <ul className={click ? ("nav-menu active") : ("nav-menu")}>
             <li>
